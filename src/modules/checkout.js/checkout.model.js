31184b1b62a40fea31170623a97f71887fb4e766
sujon-chambugong <sujonchambugong@gmail.com>
2023-09-18 14:10:39 +0600

    vercel json added

--- src/modules/checkout.js/checkout.model.js
+++ src/modules/checkout.js/checkout.model.js
@@ -37,7 +37,7 @@ const checkoutSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    DeliCharge: {
+    deliCharge: {
       type: Number,
       required: true,
     },
--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const url = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.rqtzfhi.mongodb.net/bengalbook?retryWrites=true&w=majority`;
 
 async function main() {
   try {
-    await mongoose.connect(process.env.DATA_URL, {
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
